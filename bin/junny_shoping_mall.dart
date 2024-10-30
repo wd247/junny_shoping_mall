f3a902fc6b15b546d9f22d03e7514eee303383a9
wd247 <junny3344@gmail.com>
2024-10-30 18:17:58 +0900

    추가

---
+++ bin/junny_shoping_mall.dart
@@ -0,0 +1,5 @@
+import 'package:junny_shoping_mall/junny_shoping_mall.dart' as junny_shoping_mall;
+
+void main(List<String> arguments) {
+  print('Hello world: ${junny_shoping_mall.calculate()}!');
+}
---
+++ test/junny_shoping_mall_test.dart
@@ -0,0 +1,8 @@
+import 'package:junny_shoping_mall/junny_shoping_mall.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
